bcf14ff3890db5e273869af4d318e83a17068dd5
flyship <79181438+yuxuan-fan@users.noreply.github.com>
2024-10-12 16:45:39 +0800

    DCNV2-d
    分支DCNV2-d

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
-    model.load('D:\\workspace\\github\\RTDETR-main\\weights\\rtdetr-r18.pt') # loading pretrain weights
+    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-DCNV2-Dynamic.yaml')
+    model.load('D:\\workspace\\github\\RTDETR-main\\weights\\rtdetr-r18.pt') # 预训练权重是否要用r18
     model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
@@ -12,10 +12,10 @@ if __name__ == '__main__':
                 batch=4,
                 workers=4,
                 
-                # device='0',
-                device='cpu', #本地调试使用cpu
+                device='0',
+                # device='cpu', #本地调试使用cpu
 
                 # resume='', # last.pt path
-                project='runs/train',
-                name='exp',
+                project='runs/train/',
+                name='exp-DCNV2-D',
                 )
